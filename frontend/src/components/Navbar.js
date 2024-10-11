07ac59b3772a7cd32227e42ddfd852c64eb080e6
santhoshprabhuV2003 <santhoshprabhuofficial@gmail.com>
2024-10-11 23:12:59 +0530

    first commit

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Navbar.css";
+
+const Navbar = () => {
+
+    return(
+        <nav className='navbar'>
+            <h3 className='app-name'>Cyberbullying Tweet Detection</h3>
+        </nav>
+    )
+}
+
+export default Navbar;
